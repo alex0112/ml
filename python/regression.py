706b789c6e55f1d1733acbd028f2b06294b6419a
alex0112 <alarsen0112@gmail.com>
2017-08-25 19:52:53 -0600

    Created regression file and regression object after data preprocessing.

---
+++ python/regression.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+
+
+## Get the data:
+dataset = pd.read_csv('../moar_data/Salary_Data.csv') ## Import the data.
+X = dataset.iloc[:, :-1].values  ## Select All of the values (i.e. not numbers or headers) and then remove the last column (X is the independent var)
+y = dataset.iloc[:, -1].values ## Get the dependent var (the right-most column)
+
+## Split the dataset into the training set, and test set.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+## Simple linear regression:
+regressor = LinearRegression()
+regressor.fit(X_train, y_train))
